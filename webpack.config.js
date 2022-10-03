2282d4c5bf1514eeb1e56829cd16144272ef472f
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-10-03 20:46:11 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: devMode ? "development" : "production",
+  module: {
+    rules: [
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.tsx?$/i,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/assets/index.html",
+      title: "Reddit",
+    }),
+  ]
+    .concat(devMode ? [] : [new MiniCssExtractPlugin()]),
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks.ts"),
+      "@sass": path.resolve(__dirname, "src/sass"),
+      "@store": path.resolve(__dirname, "src/store"),
+      "@types": path.resolve(__dirname, "src/types.ts"),
+    },
+    extensions: [".tsx", ".ts", ".js", ".json"],
+  },
+};
