cd74dbe481594f73ce3a687a719584b0d50aaffb
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-10-04 22:36:08 +0300

    Add basic styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 
 import { App } from "./App";
 
+import "normalize.css";
+import "@sass/_global.scss";
+
 ReactDOM.render(
   <App />,
   document.querySelector("#root")
