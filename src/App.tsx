c7ec523638a3578cd0ee3e346af702ebfd160abe
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-10-04 20:51:02 +0300

    Retrieve access token on every page load

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,36 @@ import { PostPreview } from "@components";
 export function App() {
   const [posts, setPosts] = useState([]);
   useEffect(() => {
-    fetch(
-      "https://oauth.reddit.com/r/AskReddit/hot?limit=5",
-      {
-        headers: {
-          "Authorization": "Bearer CREDENTIALS",
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((json) => setPosts(json.data.children));
+    (async () => {
+      const accessToken = await fetch(
+        "https://www.reddit.com/api/v1/access_token",
+        {
+          method: "POST",
+          body: new URLSearchParams([
+            ["grant_type", "https://oauth.reddit.com/grants/installed_client"],
+            ["device_id", "DO_NOT_TRACK_THIS_DEVICE"],
+          ]),
+          headers: {
+            "Authorization": "Basic CREDENTIALS",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((json) => json.access_token);
+
+      const posts = await fetch(
+        "https://oauth.reddit.com/r/AskReddit/hot?limit=5",
+        {
+          headers: {
+            "Authorization": `Bearer ${accessToken}`,
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((json) => json.data.children);
+
+      setPosts(posts);
+    })();
   }, []);
 
   return (
