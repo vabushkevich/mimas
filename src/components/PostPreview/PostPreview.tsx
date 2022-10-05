b5e887f481997b21fb9816ddea852012965b2825
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-10-05 22:37:12 +0300

    Style PostPreview

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Post } from "@types";
 
+import "./PostPreview.scss";
+
 export function PostPreview({
   commentCount,
   dateCreated,
@@ -11,10 +13,20 @@ export function PostPreview({
 }: Post) {
   return (
     <div className="post-preview">
-      <div className="post-preview__score">Score: {score}</div>
-      <div className="post-preview__heading">{subreddit} | Posted by {userName}. Date: {new Date(dateCreated).toLocaleString()}</div>
+      <div className="post-preview__header">
+        <div className="post-preview__subreddit">{subreddit}</div>
+        <div className="post-preview__user">{userName}</div>
+        <div className="post-preview__date">{new Date(dateCreated).toLocaleString()}</div>
+      </div>
       <div className="post-preview__title">{title}</div>
-      <div className="post-preview__comment-count">{commentCount} comments</div>
+      <div className="post-preview__footer">
+        <div className="post-preview__comments-btn">{commentCount}</div>
+        <div className="post-preview__voting">
+          <div className="post-preview__down-btn"></div>
+          <div className="post-preview__score">{score}</div>
+          <div className="post-preview__up-btn"></div>
+        </div>
+      </div>
     </div>
   );
 }
