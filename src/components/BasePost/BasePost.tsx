f26440a8d4d90289ba09b73d92c3f36df4c83dea
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-10-14 20:54:31 +0300

    Create BasePost component

---
+++ src/components/BasePost/BasePost.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Post } from "@types";
+
+import "./BasePost.scss";
+
+export function BasePost({
+  commentCount,
+  dateCreated,
+  score,
+  subreddit,
+  title,
+  url,
+  userName,
+}: Post) {
+  return (
+    <div className="post-preview">
+      <div className="post-preview__header">
+        <a href={`https://www.reddit.com/r/${subreddit}/`}>
+          <div className="post-preview__subreddit">{subreddit}</div>
+        </a>
+        <a
+          className="post-preview__user"
+          href={`https://www.reddit.com/user/${userName}/`}
+        >
+          {userName}
+        </a>
+        <div className="post-preview__date">{new Date(dateCreated).toLocaleString()}</div>
+      </div>
+      <a
+        className="post-preview__title"
+        href={url}
+      >
+        {title}
+      </a>
+      <div className="post-preview__footer">
+        <a
+          className="post-preview__comments-btn"
+          href={url}
+        >
+          {commentCount}
+        </a>
+        <div className="post-preview__voting">
+          <button className="post-preview__down-btn"></button>
+          <div className="post-preview__score">{score}</div>
+          <button className="post-preview__up-btn"></button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,12 @@ import { PostPreview } from "./PostPreview/PostPreview";
 import { PostList } from "./PostList/PostList";
 import { Container } from "./Container/Container";
 import { TestPage } from "./TestPage/TestPage";
+import { BasePost } from "./BasePost/BasePost";
 
 export {
   PostPreview,
   PostList,
   Container,
   TestPage,
+  BasePost,
 };
