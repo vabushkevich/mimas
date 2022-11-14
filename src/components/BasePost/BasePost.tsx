aa04bf284c49c2045c29fc14082750346b2da1e9
Vyacheslav Abushkevich <abshkvch@gmail.com>
2022-11-14 21:22:15 +0300

    Rename .post-preview to .post

--- src/components/BasePost/BasePost.tsx
+++ src/components/BasePost/BasePost.tsx
@@ -22,51 +22,51 @@ export function BasePost({
 }: BasePostProps) {
   return (
     <Card>
-      <div className="post-preview">
-        <div className="post-preview__header">
+      <div className="post">
+        <div className="post__header">
           <a
-            className="post-preview__subreddit"
+            className="post__subreddit"
             href={`https://www.reddit.com/r/${subreddit}/`}
           >
             <div
-              className="post-preview__subreddit-img"
+              className="post__subreddit-img"
               style={{ backgroundImage: `url("${avatar}")` }}
             ></div>
-            <div className="post-preview__subreddit-name">{subreddit}</div>
+            <div className="post__subreddit-name">{subreddit}</div>
           </a>
           <a
-            className="post-preview__user"
+            className="post__user"
             href={`https://www.reddit.com/user/${userName}/`}
           >
             {userName}
           </a>
           <div
-            className="post-preview__date"
+            className="post__date"
             title={formatDate(dateCreated)}
           >
             {formatDistanceToNow(dateCreated)}
           </div>
         </div>
         <a
-          className="post-preview__title"
+          className="post__title"
           href={url}
         >
           {title}
         </a>
         {React.Children.count(children) > 0 && (
-          <div className="post-preview__body">{children}</div>
+          <div className="post__body">{children}</div>
         )}
-        <div className="post-preview__footer">
+        <div className="post__footer">
           <a
-            className="post-preview__comments-btn"
+            className="post__comments-btn"
             href={url}
           >
             {compactNumber(commentCount)}
           </a>
-          <div className="post-preview__voting">
-            <button className="post-preview__down-btn"></button>
-            <div className="post-preview__score">{compactNumber(score)}</div>
-            <button className="post-preview__up-btn"></button>
+          <div className="post__voting">
+            <button className="post__down-btn"></button>
+            <div className="post__score">{compactNumber(score)}</div>
+            <button className="post__up-btn"></button>
           </div>
         </div>
       </div>
