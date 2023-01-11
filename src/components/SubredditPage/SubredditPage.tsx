a783fe89af3e14d6e915c43247edf1aa9aca6ad5
Vyacheslav Abushkevich <abshkvch@gmail.com>
2023-01-11 20:22:23 +0300

    Add post sorting on subreddit page

--- src/components/SubredditPage/SubredditPage.tsx
+++ src/components/SubredditPage/SubredditPage.tsx
@@ -1,30 +1,69 @@
 import React, { useState, useEffect, useContext } from "react";
 import { ClientContext } from "@context";
-import { Post } from "@types";
+import { Post, PostSortingMethod } from "@types";
 
-import { PostList, Container, Page, IntersectionDetector } from "@components";
+import {
+  PostList,
+  Container,
+  Page,
+  IntersectionDetector,
+  Card,
+  DropdownMenu,
+} from "@components";
+import "./SubredditPage.scss";
+
+const postSortingMenu: {
+  content: string;
+  value: PostSortingMethod;
+}[] = [
+  { value: "hot", content: "Hot" },
+  { value: "top", content: "Top" },
+  { value: "new", content: "New" },
+  { value: "rising", content: "Rising" },
+];
 
 export function SubredditPage() {
+  const [postSorting, setPostSorting] = useState<PostSortingMethod>("hot");
   const [posts, setPosts] = useState<Post[]>([]);
   const client = useContext(ClientContext);
   const subreddit = location.pathname.match(/\/r\/(\w+)/)[1];
+
+  const loadPosts = async (limit?: number) => {
+    const newPosts = await client.getFeedPosts({
+      limit,
+      sort: postSorting,
+      subreddit,
+    });
+    setPosts(newPosts);
+  };
 
   const loadMorePosts = async (limit?: number) => {
     const newPosts = await client.getFeedPosts({
       after: posts.at(-1)?.id,
       limit,
+      sort: postSorting,
       subreddit,
     });
     setPosts((posts) => [...posts, ...newPosts]);
   };
 
   useEffect(() => {
-    loadMorePosts(5);
-  }, []);
+    loadPosts(5);
+  }, [postSorting]);
 
   return (
     <Page>
       <Container>
+        <div className="post-sorting">
+          <Card>
+            <DropdownMenu
+              items={postSortingMenu}
+              label={({ content }) => content}
+              selectedValue={postSorting}
+              onSelect={({ value }) => setPostSorting(value)}
+            />
+          </Card>
+        </div>
         <PostList posts={posts} />
         {posts.length > 0 && (
           <IntersectionDetector
