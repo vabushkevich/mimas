09d328730b8108600df25a80346cff3d5ac8fd4b
Vyacheslav Abushkevich <abshkvch@gmail.com>
2023-03-02 20:21:26 +0300

    Move providers from App.tsx to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import { AuthContextProvider } from "@services/auth";
 
 import {
   PostPage,
@@ -16,48 +9,32 @@ import {
   AuthPage,
 } from "@components";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnMount: false,
-      refetchOnReconnect: false,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 export function App() {
   return (
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <AuthContextProvider>
-          <Router>
-            <Switch>
-              <Route path="/r/popular/:sort?">
-                <FeedPage type="popular" />
-              </Route>
-              <Route path="/r/all/:sort?">
-                <FeedPage type="all" />
-              </Route>
-              <Route path="/r/:subreddit/comments/:id">
-                <PostPage />
-              </Route>
-              <Route path="/r/:subreddit/:sort?">
-                <SubredditPage />
-              </Route>
-              <Route path="/user/:name">
-                <UserPage />
-              </Route>
-              <Route path="/auth">
-                <AuthPage />
-              </Route>
-              <Route path="/">
-                <FeedPage type="user" />
-              </Route>
-            </Switch>
-          </Router>
-        </AuthContextProvider>
-      </QueryClientProvider>
-    </Provider>
+    <Router>
+      <Switch>
+        <Route path="/r/popular/:sort?">
+          <FeedPage type="popular" />
+        </Route>
+        <Route path="/r/all/:sort?">
+          <FeedPage type="all" />
+        </Route>
+        <Route path="/r/:subreddit/comments/:id">
+          <PostPage />
+        </Route>
+        <Route path="/r/:subreddit/:sort?">
+          <SubredditPage />
+        </Route>
+        <Route path="/user/:name">
+          <UserPage />
+        </Route>
+        <Route path="/auth">
+          <AuthPage />
+        </Route>
+        <Route path="/">
+          <FeedPage type="user" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import { AuthContextProvider } from "@services/auth";
 
 import { App } from "./App";
 
 import "normalize.css";
 import "@sass/_global.scss";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 ReactDOM.render(
-  <App />,
+  (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <AuthContextProvider>
+          < App />
+        </AuthContextProvider>
+      </QueryClientProvider>
+    </Provider>
+  ),
   document.querySelector("#root")
 );
