b63f3c8dc17090d380a4d5c403895d58b76cf115
Vyacheslav Abushkevich <abshkvch@gmail.com>
2023-03-21 21:09:25 +0300

    Show smaller preview for image post

--- src/components/ImagePost/ImagePost.tsx
+++ src/components/ImagePost/ImagePost.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { findLast } from "lodash-es";
 import type { ImagePost, PostProps } from "@types";
 
 import { BasePost } from "@components";
 import "./ImagePost.scss";
 
 export function ImagePost(props: PostProps<ImagePost>) {
+  const { sizes, source } = props.post.image;
+  const preview = findLast(sizes, ({ width }) => width <= 960);
+
   return (
     <BasePost {...props}>
-      <a className="image-post-body" href={props.post.image.source.src}>
-        <img src={props.post.image.source.src} alt="" />
+      <a className="image-post-body" href={source.src}>
+        <img src={preview.src} alt="" />
       </a>
     </BasePost>
   );
