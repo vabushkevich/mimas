558b24ac2b4f7d5c2e5a97ae6c58de1ec7b7caaf
Vyacheslav Abushkevich <abshkvch@gmail.com>
2023-06-15 21:43:09 +0300

    Debounce handleError()

--- src/services/query-client.tsx
+++ src/services/query-client.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryCache, MutationCache } from "@tanstack/react-query";
 import toast from "react-hot-toast";
+import { debounce } from "lodash-es";
 
 function handleError() {
   toast.error("Something went wrong");
@@ -14,9 +15,9 @@ export const queryClient = new QueryClient({
     },
   },
   mutationCache: new MutationCache({
-    onError: handleError,
+    onError: debounce(handleError),
   }),
   queryCache: new QueryCache({
-    onError: handleError,
+    onError: debounce(handleError),
   }),
 });
