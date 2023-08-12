efd893971e05dbb3735fdbbeb1d349c7ea13ceaa
Vyacheslav Abushkevich <abshkvch@gmail.com>
2023-08-12 22:11:41 +0300

    Use usePostParams in PostPage

--- src/components/PostPage/PostPage.tsx
+++ src/components/PostPage/PostPage.tsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import { useQueryString } from "@hooks";
-import { createId } from "@utils";
-import { isCommentSortingOption } from "@types";
+import { usePostParams } from "@hooks";
 import { usePost, usePostComments } from "@services/api";
 
 import { Post, Container, Page, PostSkeleton } from "@components";
@@ -11,12 +8,7 @@ import { PostCommentsSkeleton } from "./PostCommentsSkeleton";
 import "./PostPage.scss";
 
 export function PostPage() {
-  const query = useQueryString<{ sort: string }>();
-  const commentsSorting = isCommentSortingOption(query.sort)
-    ? query.sort
-    : "confidence";
-  const params = useParams<{ id: string }>();
-  const postId = createId(params.id, "post");
+  const { postId, sort: commentsSorting } = usePostParams();
   const { data: post, isLoading: isPostLoading } = usePost(postId);
   const { data: threadList, isLoading: isCommentsLoading } = usePostComments(
     postId,
