b6ddcb9aaabaa2664f0c0ece96a1af6eed19575a
Vyacheslav Abushkevich <abshkvch@gmail.com>
2023-10-13 22:43:31 +0300

    Unmount non-selectable dropdown menu on close

--- src/components/DropdownMenu/DropdownMenu.tsx
+++ src/components/DropdownMenu/DropdownMenu.tsx
@@ -47,6 +47,7 @@ export function DropdownMenu({
         in={isOpen}
         nodeRef={menuRef}
         timeout={100}
+        unmountOnExit={!menuProps.selectable}
       >
         {(state) => (
           <div
