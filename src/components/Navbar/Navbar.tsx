35e98d16244290e1728ab2102f522ddf279003fd
Vyacheslav Abushkevich <abshkvch@gmail.com>
2024-01-30 21:53:41 +0300

    Don't enlarge navbar when search section is open

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import { useIdentity } from "@services/api";
 import { NavbarLogo } from "@components";
 import { UserMenu } from "./UserMenu";
 import { Search } from "./Search";
+import ArrowLeftIcon from "@assets/svg/arrow-left.svg";
 import MagnifierIcon from "@assets/svg/magnifier.svg";
 import "./Navbar.scss";
 
@@ -17,40 +18,47 @@ export function Navbar({ onMenuButtonClick }: NavbarProps) {
   const { authorized } = useAuth();
   const [isSearchMode, setIsSearchMode] = useState(false);
   const isSmallScreen = useMediaQuery("(max-width: 576px)");
-  const searchButtonRef = useRef<HTMLButtonElement>(null);
-  const searchRef = useRef<HTMLDivElement>(null);
+  const searchSectionRef = useRef<HTMLDivElement>(null);
   const { data: identity } = useIdentity({ enabled: authorized });
   const user = authorized ? identity?.user : undefined;
 
-  useClickOutside([searchButtonRef, searchRef], () => setIsSearchMode(false));
+  useClickOutside(searchSectionRef, () => setIsSearchMode(false));
 
   return (
     <nav className="site-nav">
-      <div className="site-nav__row site-nav__row--main">
-        <NavbarLogo onMenuButtonClick={onMenuButtonClick} />
-        <div className="site-nav__right-col">
-          {isSmallScreen ? (
-            <button
-              className="site-nav__search-btn"
-              ref={searchButtonRef}
-              onClick={() => setIsSearchMode(!isSearchMode)}
-            >
-              <MagnifierIcon width="20" />
-            </button>
-          ) : (
-            <div className="site-nav__search">
-              <Search />
-            </div>
-          )}
-          <div className="site-nav__user-menu">
-            <UserMenu user={user} />
+      {isSmallScreen && isSearchMode ? (
+        <div className="site-nav__section" ref={searchSectionRef}>
+          <button
+            className="site-nav__button site-nav__section-back-btn"
+            onClick={() => setIsSearchMode(false)}
+          >
+            <ArrowLeftIcon width="18" />
+          </button>
+          <div className="site-nav__section-content">
+            <Search autoFocus onSubmit={() => setIsSearchMode(false)} />
           </div>
         </div>
-      </div>
-      {isSmallScreen && isSearchMode && (
-        <div className="site-nav__row site-nav__row--search" ref={searchRef}>
-          <Search autoFocus onSubmit={() => setIsSearchMode(false)} />
-        </div>
+      ) : (
+        <>
+          <NavbarLogo onMenuButtonClick={onMenuButtonClick} />
+          <div className="site-nav__right-col">
+            {isSmallScreen ? (
+              <button
+                className="site-nav__button"
+                onClick={() => setIsSearchMode(true)}
+              >
+                <MagnifierIcon width="20" />
+              </button>
+            ) : (
+              <div className="site-nav__search">
+                <Search />
+              </div>
+            )}
+            <div className="site-nav__user-menu">
+              <UserMenu user={user} />
+            </div>
+          </div>
+        </>
       )}
     </nav>
   );
